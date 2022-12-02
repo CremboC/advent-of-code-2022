4ca26a9b51758bd9cb301faf489608ffc47f20c8
Paulius Imbrasas <paulius@permutive.com>
2022-12-02 17:37:08 +0000

    Clean-up day 2

--- src/day02.rs
+++ src/day02.rs
@@ -3,6 +3,41 @@ use nom::character::complete::{alpha1, char};
 use nom::sequence::separated_pair;
 use std::fs;
 
+#[derive(Clone, Copy)]
+enum Hand {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl Hand {
+    fn parse(s: &str) -> Hand {
+        match s {
+            "A" | "X" => Hand::Rock,
+            "B" | "Y" => Hand::Paper,
+            "C" | "Z" => Hand::Scissors,
+            _ => panic!("No hand match"),
+        }
+    }
+}
+
+enum State {
+    Lose = 0,
+    Draw = 3,
+    Win = 6,
+}
+
+impl State {
+    fn parse(s: &str) -> State {
+        match s {
+            "X" => State::Lose,
+            "Y" => State::Draw,
+            "Z" => State::Win,
+            _ => panic!("No state match"),
+        }
+    }
+}
+
 fn parser(input: &str) -> nom::IResult<&str, (&str, &str)> {
     let mut parser = separated_pair(alpha1, char(' '), alpha1);
     return parser(input);
@@ -11,38 +46,23 @@ fn parser(input: &str) -> nom::IResult<&str, (&str, &str)> {
 pub fn part1() {
     let input = fs::read_to_string("inputs/day02.txt").expect("Failed to read file.");
 
+    fn play(h1: Hand, h2: Hand) -> State {
+        match (h1, h2) {
+            (Hand::Paper, Hand::Rock) => State::Win,
+            (Hand::Rock, Hand::Scissors) => State::Win,
+            (Hand::Scissors, Hand::Paper) => State::Win,
+            (Hand::Rock, Hand::Paper) => State::Lose,
+            (Hand::Scissors, Hand::Rock) => State::Lose,
+            (Hand::Paper, Hand::Scissors) => State::Lose,
+            (_, _) => State::Draw,
+        }
+    }
+
     let score = input.split("\n").fold(0, |score, line| {
         let (_, (l, r)) = parser(line).expect("Failed to parse");
-        let add = match r {
-            "X" => {
-                1 + match l {
-                    "A" => 3,
-                    "B" => 0,
-                    "C" => 6,
-                    _ => 0,
-                }
-            }
-            "Y" => {
-                // paper
-                2 + match l {
-                    "A" => 6,
-                    "B" => 3,
-                    "C" => 0,
-                    _ => 0,
-                }
-            }
-            "Z" => {
-                // scissors
-                3 + match l {
-                    "A" => 0,
-                    "B" => 6,
-                    "C" => 3,
-                    _ => 0,
-                }
-            }
-            _ => 0,
-        };
-        score + add
+        let left_hand = Hand::parse(l);
+        let right_hand = Hand::parse(r);
+        score + (right_hand as i32) + (play(right_hand, left_hand) as i32)
     });
 
     println!("Result {score}");
@@ -50,17 +70,6 @@ pub fn part1() {
 }
 
 pub fn part2() {
-    enum Hand {
-        Rock = 1,
-        Paper = 2,
-        Scissors = 3,
-    }
-
-    enum State {
-        Lose = 0,
-        Draw = 3,
-        Win = 6,
-    }
     let input = fs::read_to_string("inputs/day02.txt").expect("Failed to read file.");
 
     fn get_hand(h: Hand, s: &State) -> Hand {
@@ -77,19 +86,8 @@ pub fn part2() {
 
     let score = input.split("\n").fold(0, |score, line| {
         let (_, (l, r)) = parser(line).expect("Failed to parse");
-        let hand = match l {
-            "A" => Hand::Rock,
-            "B" => Hand::Paper,
-            "C" => Hand::Scissors,
-            _ => panic!("No hand match"),
-        };
-        let state = match r {
-            "X" => State::Lose,
-            "Y" => State::Draw,
-            "Z" => State::Win,
-            _ => panic!("No state match"),
-        };
-
+        let hand = Hand::parse(l);
+        let state = State::parse(r);
         let hand_to_play = get_hand(hand, &state);
         score + (hand_to_play as i32) + (state as i32)
     });
